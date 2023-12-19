56a1ecaf4509543a082430a78983560e98f3a67a
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-18 23:43:08 -0600

    Number challenge completed

--- 01-variables-data-types/13-number-challenge/script.js
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -1,1 +1,9 @@
-console.log('Challenge')
+const x = Math.floor(Math.random() * 100) + 1
+const y = Math.floor(Math.random() * 50) + 1
+console.log(x + ' ' + y)
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
