9862bd243f21240abee73f8ae2930fdb66e7abd8
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-20 23:52:51 -0600

    Array challenge completed

--- 02-arrays-and-objects/04-array-challenge/script.js
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -1,1 +1,12 @@
-console.log('Challenge')
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr.concat(6).reverse().concat(0))
+// [6, 5, 4, 3, 2, 1, 0];
+
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [5, 6, 7, 8, 9, 10]
+
+const arr3 = arr1.splice(0, 4).concat(arr2)
+
+console.log(arr3)
+// [1,2,3,4,5,6,7,8,9,10]
