e54a1e9c730f2fe618c150e9ffcf700c83869593
Brian Shimkus <brianshimkus1@gmail.com>
2024-01-05 00:31:58 -0600

    Creating a function

---
+++ 03-functions-scope/script.js
@@ -0,0 +1,20 @@
+function sayHello() {
+	console.log('Hello')
+}
+
+sayHello()
+
+function add(num1, num2) {
+	console.log(num1 + num2)
+}
+
+add(5, 10)
+
+function subtract(num1, num2) {
+	return num1 - num2
+
+	console.log('This will not be printed')
+}
+
+const result = subtract(10, 2)
+console.log(result)
