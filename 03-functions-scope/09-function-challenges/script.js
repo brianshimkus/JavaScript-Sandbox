761d1468f759922852b284a55594a4716ec5717e
Brian Shimkus <brianshimkus1@gmail.com>
2024-01-18 00:22:26 -0600

    Function challenge completed

--- 03-functions-scope/09-function-challenges/script.js
+++ 03-functions-scope/09-function-challenges/script.js
@@ -1,1 +1,21 @@
-console.log('Hello')
+// Challenge 1
+const getCelsius = (fahrenheit) => (fahrenheit - 32) * (5 / 9)
+console.log(`The temperature is ${getCelsius(32)} \xB0C`)
+
+// Challenge 2
+function minMax(arr) {
+	const min = Math.min(...arr)
+	const max = Math.max(...arr)
+	return { min, max }
+}
+console.log(minMax([1, 2, 3, 4, 5]))
+
+// Challenge 3
+function calculateArea(l, w) {
+	area = l * w
+
+	console.log(
+		`The area of a rectangle with a length of ${l} and a width of ${w} is ${area}.`
+	)
+}
+calculateArea(5, 10)
