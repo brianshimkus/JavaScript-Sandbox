18079ec50fceefbd61a7bfd71baea8a111e79751
Brian Shimkus <brianshimkus1@gmail.com>
2024-01-20 00:26:35 -0600

    Calculator challenge completed

--- 04-logic-control-flow/04-calculator-challenge/script.js
+++ 04-logic-control-flow/04-calculator-challenge/script.js
@@ -1,1 +1,17 @@
-console.log('Calculator Challenge')
+function calculator(num1, num2, operator) {
+	if (operator === '+') {
+		console.log(num1 + num2)
+	} else if (operator === '-') {
+		console.log(num1 - num2)
+	} else if (operator === '*') {
+		console.log(num1 * num2)
+	} else if (operator === '/') {
+		console.log(num1 / num2)
+	} else if (operator === '%') {
+		console.log(num1 % num2)
+	} else {
+		console.log('Invalid operator')
+	}
+}
+
+calculator(5, 15, '*')
